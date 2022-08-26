cba6c2b6bc08d9886f9fcda29a3d40af7b4f73e8
manielsen13 <marshall.niels@gmail.com>
2022-08-26 16:50:57 -0400

    Create Sketch Pad Screen
    Make functions that create the grid for sketch pad screen. Adjust HTML by adding div for the screen. Style layout of grid.

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+//create 16 rows of divs and make them show in flex with a column direction
+//add 16 squares per row and make them show in a row direction
+
+//need them to start out white with a black border that doesn't take up extra space or add onto the width and height
+//change to no border and fill the background with color.
+
+const sketchPadScreen = document.querySelector(".sketchPadScreen");
+
+const numRows = 16;
+const numColumns = 16;
+
+addRows();
+addSquares();
+
+function createRow() {
+    const row = document.createElement("row");
+    row.style.display = "flex";
+    return row;
+}
+
+function addRows () {
+    for (let i = 0; i < 16; i++) {
+        const row = createRow()
+        sketchPadScreen.appendChild(row)
+    }
+}
+
+function addSquares() {
+    const rowList = document.querySelectorAll("row");
+    for (let i = 0; i < 16; i++) {
+        for (let j = 0; j < 16; j++) {
+            const square = createSquare();
+            rowList[i].appendChild(square);
+        }
+    }
+}
+
+
+function createSquare() {
+    console.log("making square...")
+    const square = document.createElement("square");
+    square.style.borderStyle = "solid";
+    square.style.borderColor = "black";
+    square.style.minHeight = "20px";
+    square.style.minWidth = "20px";
+    square.style.boxSizing = "border-box";
+    square.style.borderWidth = "thin";
+    return square;
+}
+
+
 /*
 
 //round is played when buttons are pressed, unless a player has reached 5--then it resets
